0d6a2ce3bbbd3fc939f2672f34593d51c01ab95f
Martin Kregar <martinkregar+uol@gmail.com>
2026-03-31 22:59:14 +0200

    still not long enough

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  timeout: 60_000,
+  timeout: 120_000,
   use: {
     baseURL: 'https://staging.nepremicnine.btc-city.com/ljubljana',
     browserName: 'chromium',
