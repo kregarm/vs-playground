8cf1010eae625ccee267ecc4a1540da96258e2ba
Martin Kregar <martinkregar+uol@gmail.com>
2026-03-31 21:51:11 +0200

    fix name, remove timeout - running in one worker solved the issue

--- tests/screenshot.spec.ts
+++ tests/screenshot.spec.ts
@@ -1,14 +1,12 @@
 import { test, expect } from '../fixtures/pages';
 
 test.describe('Screenshoting tests', () => {
-    test('User can filter listings and open details', async ({ landingPage, page }) => {
+    test('Result card', async ({ landingPage }) => {
         const resultCard = landingPage.resultCardByTitleAndPrice('Trgovina', 'Po dogovoru');
 
         await resultCard.expectVisible();
 
-        await expect(resultCard.card()).toHaveScreenshot('result-card.png', {
-            timeout: 15000
-        });
+        await expect(resultCard.card()).toHaveScreenshot('result-card.png');
     });
 })
 
